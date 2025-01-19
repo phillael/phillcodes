cdde3803cd6ed4d65a40dbe797a44a07b67cf44b
Phill Aelony <phillael@yahoo.com>
2025-01-19 12:54:58 -0600

    Update dashboard menu, Bio, experience, and styles

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,16 +1,43 @@
 import Header from './Heading'
-import HeroImage from './HeroImage'
+import Bio from './RpgMenu/Bio'
 import RPGMenu from './RpgMenu/RpgMenu'
+
+const menuItems = [
+  {
+    id: 'bio',
+    label: 'Bio',
+    children: <Bio />,
+  },
+  {
+    id: 'status',
+    label: 'Status',
+    children: <div>Status</div>,
+  },
+  {
+    id: 'skills',
+    label: 'Skills',
+    children: <div>Skills</div>,
+  },
+  {
+    id: 'weapons',
+    label: 'Equip',
+    children: <div>Equip</div>,
+  },
+  {
+    id: 'music',
+    label: 'Music',
+    children: <div>Music</div>,
+  },
+]
 
 const Dashboard = () => {
   return (
-    <section className="h-screen flex flex-row basis-1/2">
-      <div className="flex flex-col justify-between p-8 w-full overflow-hidden">
+    <section className="h-screen p-8 lg:basis-1/2">
+      <div className="flex flex-col h-full">
         <Header />
-        <div className="flex-grow flex flex-col justify-center overflow-hidden rounded-full">
-          <HeroImage />
+        <div className="flex-1 min-h-0">
+          <RPGMenu items={menuItems} />{' '}
         </div>
-        <RPGMenu />
       </div>
     </section>
   )
